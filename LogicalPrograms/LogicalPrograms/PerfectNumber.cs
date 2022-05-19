c893bf55bf9878449e78549a582c3e5b205bf998
Suraj-Dal <dsurajdal10@gmail.com>
2022-05-19 23:42:57 +0530

    [Suraj]: Program to find number is perfect or not is added.

---
+++ LogicalPrograms/LogicalPrograms/PerfectNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class PerfectNumber
+    {
+        public void perfect()
+        {
+            int number, sum = 0, n;
+            Console.WriteLine("Enter the Number:");
+            number = Convert.ToInt32(Console.ReadLine());
+            
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    sum = sum + i;
+                    if (sum != number)
+                    Console.WriteLine(sum);
+                }
+            }
+            if (sum == number)
+                Console.WriteLine("Number is perfect number.");
+            else
+                Console.WriteLine("Number is not a perfect number.");
+        }
+    }
+}
--- LogicalPrograms/LogicalPrograms/Program.cs
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -1,2 +1,14 @@
-﻿LogicalPrograms.Fabonacci fab = new LogicalPrograms.Fabonacci();
-fab.series();
+﻿Console.WriteLine("1.Fabonacci Series\n2.Perfect Number\n");
+Console.WriteLine("Enter your choice:");
+int choice = Convert.ToInt32(Console.ReadLine());
+switch (choice)
+{
+    case 1:
+        LogicalPrograms.Fabonacci fab = new LogicalPrograms.Fabonacci();
+        fab.series();
+        break;
+    case 2:
+        LogicalPrograms.PerfectNumber per = new LogicalPrograms.PerfectNumber();
+        per.perfect();
+        break;
+}
