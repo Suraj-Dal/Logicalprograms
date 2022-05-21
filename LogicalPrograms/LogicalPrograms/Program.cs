e562a638738135e8142deadfc08f06b2eb0b0af7
Suraj-Dal <dsurajdal10@gmail.com>
2022-05-21 20:39:28 +0530

    [Suraj]: Usecase 10 to convert Decimal to Binary is added.

--- LogicalPrograms/LogicalPrograms/Program.cs
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("1.Fabonacci Series\n2.Perfect Number\n3.Prime Number\n4.Reverse Number\n5.Stop Watch\n6.Coupon Number\n7.Day of Week\n8.Temperatue Conversion\n9.Monthly Payment\n");
+﻿Console.WriteLine("1.Fabonacci Series\n2.Perfect Number\n3.Prime Number\n4.Reverse Number\n5.Stop Watch\n6.Coupon Number\n7.Day of Week\n8.Temperatue Conversion\n9.Monthly Payment\n10.Binary Conversion");
 Console.WriteLine("Enter your choice:");
 int choice = Convert.ToInt32(Console.ReadLine());
 switch (choice)
@@ -37,6 +37,9 @@ switch (choice)
         break;
     case 9:
         LogicalPrograms.monthlyPayment.calculateEMI();
+        break;
+    case 10:
+        LogicalPrograms.Binary.getBinary();
         break;
     default:
         Console.WriteLine("Enter valid option.");
